4b4358ff39321c1bd16efc13cef11d5d2b658982
cochindreamdestinations <cochindreamdestinations@gmail.com>
2024-10-09 17:03:13 +0000

    layout fix for page

--- src/app/layout.js
+++ src/app/layout.js
@@ -62,26 +62,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="twitter:title" content="Sachit Kurup"></meta>
-        <meta
-          property="twitter:image"
-          content="./images/hero-image.jpeg"
-        ></meta>
-        <meta property="twitter:card" content="/images/hero-image.jpeg"></meta>
-        <meta property="og:title" content="Sachit Kurup"></meta>
-        <meta property="og:site_name" content="sachitkurup.in"></meta>
-        <meta property="og:url" content="sachitkurup.in"></meta>
-        <meta
-          property="og:description"
-          content="Musician | Voilinst | Bansuri"
-        ></meta>
-        <meta property="og:type" content=""></meta>
-        <meta
-          property="og:image"
-          content="sachitkurup.vercel.app/images/hero-image.jpeg"
-        ></meta>
-      </Head>
+     
       <body className={inter.className}>{children}</body>
     </html>
   );
