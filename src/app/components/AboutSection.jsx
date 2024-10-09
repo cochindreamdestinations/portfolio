254cd17839ae480c7194bac6c5fb94566201982e
cochindreamdestinations <cochindreamdestinations@gmail.com>
2024-10-09 10:37:26 +0000

    Adding new ui to portfolio

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,93 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Violin</li>
+        <li>Bansuri</li>
+        <li>Carnatic Flute</li>
+        <li>Vocals</li>
+        
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Music Academy</li>
+        <li>Kochi</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Achievements",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>IAGB Youth Icon Award</li>
+        <li>Sa Re Ga Ma Pa</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          Sachit Kurup's musical journey began at a young age on the violin,bansuri and vocals. For five years he rigorously trained in Carnatic flute under Guru Shri Karthik Krishna. Sachit's dedication and talent are recognize through accolades including winning for Hindi Manch Sa Re Ga Ma Pa competition at 7. He recently reveived the prestigious IAGB Youth Icon Award in 2023, solidifying his status as a rising star. Sachit's mastery of multiple disciplines and unwavering commit ment havemade his a versatile accomplished young artiste. As he continues honing his Carnatic skills Sachit remains dedicated to pushing boundaries and inspiring generations, his journey testifying to the power of music and youthful potential
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,126 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Jab Deep Jale",
+    description: "Flute Cover",
+    image: "/images/projects/1.png",
+    tag: ["All", "Solo"],
+    previewUrl: "https://www.youtube.com/watch?v=JC_BXEwf0gY",
+  },
+  {
+    id: 2,
+    title: "Concert Kochi",
+    description: "Music for Charity Project",
+    image: "/images/projects/2.webp",
+    tag: ["All", "Concert"],
+    
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Bansuri Indian Flute",
+    description: "Bansuri Indian flute presentation",
+    image: "/images/projects/3.webp",
+    tag: ["All", "Solo"],
+    
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Saathiya Tune Kya Kiya (cover)",
+    description: "Saathiya Tune Kya Kiya (cover)",
+    image: "/images/projects/4.webp",
+    tag: ["All", "Concert"],
+    
+    previewUrl: "https://www.youtube.com/watch?v=nKggbBMtxc4",
+  },
+  {
+    id: 5,
+    title: "Celebration",
+    description: "Authentication and CRUD operations",
+    image: "/images/projects/5.webp",
+    tag: ["All", "Solo"],
+    
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Jeevamshamayi live",
+    description: "Solace Charity fundraiser, Dec 2021",
+    image: "/images/projects/6.webp",
+    tag: ["All", "Solo"],
+    
+    previewUrl: "https://www.youtube.com/watch?v=OXEs_gMG37Q",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Solo"
+          isSelected={tag === "Solo"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Concert"
+          isSelected={tag === "Concert"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
