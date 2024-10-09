c698bba353bdec6dd7a1fb1f38f9de4db6c0a0f5
cochindreamdestinations <cochindreamdestinations@gmail.com>
2024-10-09 17:01:31 +0000

    Adding meta data for fetching

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,59 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sachit Kurup ",
-  description: "Musician | Flute player | Violinst",
+  
+  title: 'Sachit Kurup | Musician',
+  description: 'Young musician with talents in Vocal, Flute, Violin',
+  twitter: {
+    card: 'https://sachitkurup.vercel.app/hero-image.jpeg',
+    url: 'https://sachitkurup.vercel.app',
+    title: 'Sachit Kurup | Musician',
+    description: 'Young musician with talents in Vocal, Flute, Violin',
+  },
   openGraph: {
-    title: "Sachit Kurup",
-    content: "/images/hero-image.jpeg",
+    title: 'Sachit Kurup | Musician',
+    description: 'Young musician with talents in Vocal, Flute, Violin',
+    url: 'https://sachitkurup.vercel.app',
+    siteName: 'Cochin Dream Destinations Taxi Service',
+    images: [
+      {
+        url: 'https://sachitkurup.vercel.app/hero-image.jpeg', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://sachitkurup.vercel.app/hero-image.jpeg', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    videos: [
+      {
+        url: 'https://nextjs.org/video.mp4', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    audio: [
+      {
+        url: 'https://nextjs.org/audio.mp3', // Must be an absolute URL
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
 };
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
+
 
 export default function RootLayout({ children }) {
   return (
